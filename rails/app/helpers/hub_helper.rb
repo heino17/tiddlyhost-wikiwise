58d13a4cca8db8eefd3c91e222686cdd86df198a
heino17 <tino@das-buddhistische-haus.de>
2025-12-29 01:17:51 +0100

    translation

--- rails/app/helpers/hub_helper.rb
+++ rails/app/helpers/hub_helper.rb
@@ -17,8 +17,8 @@ module HubHelper
 
   def views_and_updated_text(hub_site, show_clone_count: false)
     [
-      ("#{nice_view_count(hub_site.view_count)} views" unless show_clone_count),
-      ("#{nice_view_count(hub_site.clone_count)} clones" if show_clone_count),
+      ("#{nice_view_count(hub_site.view_count)} Aufrufe" unless show_clone_count),
+      ("#{nice_view_count(hub_site.clone_count)} x geklont" if show_clone_count),
       (brief_time_ago_in_words(hub_site.blob_created_at) if hub_site.blob_created_at && hub_site.save_count > 0)
     ].compact.join(', ')
   end
