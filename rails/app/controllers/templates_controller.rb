fec1a645dad67b02f0c9f36b812dab504960b396
heino17 <tino@das-buddhistische-haus.de>
2026-01-10 15:44:31 +0100

    preparation and translation for i18n

--- rails/app/controllers/templates_controller.rb
+++ rails/app/controllers/templates_controller.rb
@@ -3,12 +3,7 @@ class TemplatesController < HubController
 
   def set_default_title
     @thing_name = 'Template'
-    @default_title = 'Templates'
-    @explanation_text = %(
-      Templates are useful or interesting customized editions of TiddlyWiki, created by
-      Tiddlyhost users, that you can copy and use for yourself. Click "Clone" to create
-      a site on Tiddlyhost using a template, or click "Download" to download a copy to
-      use locally.
-    ).squish
+    @default_title = t('site_templates_title')
+    @explanation_text = t('site_templates_explanation_text').squish
   end
 end
