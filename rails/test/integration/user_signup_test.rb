0bbebafb12a4e8fd499d6d027756aaef2efd323d
heino17 <tino@das-buddhistische-haus.de>
2026-02-17 16:06:56 +0100

    fix: make user signup test robust against i18n and link visibility

--- rails/test/integration/user_signup_test.rb
+++ rails/test/integration/user_signup_test.rb
@@ -50,7 +50,8 @@ class UserSignupTest < CapybaraIntegrationTest
     assert page.has_css?('h1', text: I18n.t('your_sites'))
 
     # Logout
-    click_link I18n.t('user_menu_item_logout')
+    # click_link I18n.t('user_menu_item_logout')
+    find("a[href='/users/sign_out'][data-method='delete']").click
 
     # Confirm we are logged out
     assert page.has_css?('.jumbotron')
