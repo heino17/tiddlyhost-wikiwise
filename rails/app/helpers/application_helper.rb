dc33516b1bf318bad44a0b1c7f59de51a83073e0
heino17 <tino@das-buddhistische-haus.de>
2026-02-24 23:50:30 +0100

    The language switch now displays American instead of English.

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def flag_code_for(locale)
     case locale.to_sym
-    when :en then 'gb' # oder 'us' – deine Wahl!
+    when :en then 'us' # oder 'us' – deine Wahl!
     when :de then 'de'
     when :es then 'es'
     when :fr then 'fr'
