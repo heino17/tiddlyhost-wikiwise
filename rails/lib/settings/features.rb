391659bef1758054f28320c50024188cd1f2d259
heino17 <tino@das-buddhistische-haus.de>
2026-03-12 22:22:56 +0100

    Added switch to prevent registration, added notification box that can be turned on, both switches in features.rb

--- rails/lib/settings/features.rb
+++ rails/lib/settings/features.rb
@@ -20,4 +20,26 @@ module Settings::Features
   def redirect_tspot_to_url_enabled?(user)
     user&.is_admin? || user&.has_subscription?
   end
+
+  def startpage_introduction_block_enabled?
+    # Variante A – schnell & hartcodiert zum Testen
+    false   # oder false
+
+    # Variante B – später aus DB (empfohlen)
+    # GlobalSetting.startpage_introduction_block_enabled?   # oder Setting[:startpage_introduction_block_enabled]
+
+    # Variante C – aus Rails config (deploy nötig bei Änderung)
+    # Rails.application.config.x.startpage_introduction_block_enabled || false
+  end
+
+  def registration_enabled?
+    # Variante A – schnell & hartcodiert zum Testen
+    true   # oder false
+
+    # Variante B – später aus DB (empfohlen)
+    # GlobalSetting.registration_enabled?   # oder Setting[:registration_enabled]
+
+    # Variante C – aus Rails config (deploy nötig bei Änderung)
+    # Rails.application.config.x.registration_enabled || false
+  end
 end
